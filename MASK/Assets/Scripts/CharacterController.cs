9bc2bc486a8f187ab424bfcebceea730aa16fdbe
Saki <72980957+GameDevAssetForge@users.noreply.github.com>
2026-01-30 19:38:17 -0500

    Update CharacterController.cs

--- MASK/Assets/Scripts/CharacterController.cs
+++ MASK/Assets/Scripts/CharacterController.cs
@@ -1,7 +1,25 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
+    public InputActionAsset InputActions;
+
+    private InputAction i_moveAction;
+    private InputAction i_lookAction;
+    private InputAction i_interactAction;
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
